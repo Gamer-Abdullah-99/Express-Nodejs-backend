e95a038ac7a0be5db71732252be47671ad903470
Gamer-Abdullah-99 <sabn730@gmail.com>
2023-10-12 18:38:34 +0500

    Connected DB to backend

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,23 +1,25 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get All Customers
 //@route GET /customers
 //@access public
-const getCustomers = (req, res) => {
+const getCustomers = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Customers Successfully" });
-};
+});
 
 //@desc Get Customer By Id
 //@route GET /customers/:id
 //@access public
-const getCustomerById = (req, res) => {
+const getCustomerById = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Get Customer By Id Successfully ${req.params.id}` });
-};
+});
 
 //@desc Add Customer
 //@route POST /customers
 //@access public
-const addCustomer = (req, res) => {
+const addCustomer = asyncHandler(async (req, res) => {
   console.log("New Customer data :", req.body);
   const { name, contact, address } = req.body;
   if (!name || !contact || !address) {
@@ -25,25 +27,25 @@ const addCustomer = (req, res) => {
     throw new Error("All Fields must be filled");
   }
   res.status(201).json({ message: "Add Customer Successfully" });
-};
+});
 
 //@desc Update Customer
 //@route PUT /customers
 //@access public
-const updateCustomer = (req, res) => {
+const updateCustomer = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Update Customer Successfully ${req.params.id}` });
-};
+});
 
 //@desc Delete Customer
 //@route DELETE /customers
 //@access public
-const deleteCustomer = (req, res) => {
+const deleteCustomer = asyncHandler(async (req, res) => {
   res
     .status(200)
     .json({ message: `Delete Customer Successfully ${req.params.id}` });
-};
+});
 
 module.exports = {
   getCustomers,
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+connectDB();
 const app = express();
 
 const port = process.env.PORT || 5001;
@@ -17,3 +20,5 @@ app.listen(port, () => {
 app.use(express.json());
 
 app.use("/customers", require("./routes/customers"));
+
+app.use(errorHandler);
